f171a7a1cb07997df05274c8253c8f6b7aa5c3c3
FanteyOfficial <lorida1602@gmail.com>
2022-10-09 17:44:39 +0200

    Update

--- Assets/Scripts/FinalWindowCommands.cs
+++ Assets/Scripts/FinalWindowCommands.cs
@@ -5,6 +5,12 @@ using UnityEngine.SceneManagement;
 
 public class FinalWindowCommands : MonoBehaviour
 {
+    void Start()
+    {
+        Cursor.lockState = CursorLockMode.None;
+        Cursor.visible = true;
+    }
+
     public void backMenu()
     {
         SceneManager.LoadScene("StartMenu");
